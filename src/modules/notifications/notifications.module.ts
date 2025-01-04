2b2566b779464f889baee0adad374f1248faf8e9
Kevin <kevin.rivaslive@gmail.com>
2025-01-04 00:15:38 -0600

    fix: remove notification consumer

--- src/modules/notifications/notifications.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { NotificationsConsumer } from './notifications.consumer';
-
-@Module({
-  imports: [ConfigModule],
-  providers: [NotificationsConsumer],
-})
-export class NotificationsModule {}
